17b1fc61d9bfbbd01cb5fd6010727202c582ad74
Ana López <anamuyamiga@gmail.com>
2023-11-24 17:39:18 +0100

    xdd

--- interfaz.py
+++ interfaz.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, render_template, redirect, session
-import calculadora as calcu
+from calculadora import Calculadora
 
 app = Flask(__name__)
 app.secret_key = 'secret_key'
@@ -8,43 +8,31 @@ app.secret_key = 'secret_key'
 def calculator():
     return render_template('calculadora.html')
 
-@app.route('/identificar_operacion', methods=['POST'])
-def identificar_operacion():
-    if request.method == 'POST':
-        operacion = request.form.get('operacion')
-        numeros_input = request.form.get('inputNumbers')
-        
-        numeros = numeros_input.split()
-        
-        # Almacena la operación en la sesión
-        session['operacion'] = operacion
-        
-        return render_template('calculadora.html', input_numbers=numeros_input, operacion=operacion) 
-
 @app.route('/realizar_operacion', methods=['POST'])
 def realizar_operacion():
     if request.method == 'POST':
-        numeros_input = request.form.get('inputNumbers')
-        operacion = session.get('operacion', 'igual')  # Obtén la operación almacenada en la sesión
-        
-        numeros = numeros_input.split()
-        
-        calculadora = calcu.Calculadora(numeros[0], numeros[1])
-        
+        numero1 = request.form.get('numero1')
+        numero2 = request.form.get('numero2')
+        operacion = request.form.get('operacion')
+
+        # Almacena la operación en la sesión
+        session['operacion'] = operacion
+
+        calculadora = Calculadora(numero1, numero2)
+
         resultado = None
         if operacion == 'sumar':
-            resultado = calculadora.suma()        
+            resultado = calculadora.suma()
         elif operacion == 'restar':
             resultado = calculadora.resta()
         elif operacion == 'multiplicar':
             resultado = calculadora.multiplicacion()
         elif operacion == 'division':
             resultado = calculadora.division()
-        else: 
+        else:
             resultado = "Operacion no reconocida"
-            
-        return render_template('calculadora.html', input_numbers=f"{numeros[0]} {numeros[1]} {resultado}", operacion=operacion) 
 
+        return render_template('calculadora.html', numero1=numero1, numero2=numero2, resultado=resultado, operacion=operacion)
 
 if __name__ == '__main__':
     app.run(debug=True)
